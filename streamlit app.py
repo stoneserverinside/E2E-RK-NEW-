d1c1306d9fca92bd06ac469c02773f05eb3e0a18
stoneserverinside <stoneserver435@gmail.com>
2026-01-03 12:31:38 +0300

    Update streamlit app.py

--- streamlit app.py
+++ streamlit app.py
@@ -512,7 +512,8 @@ def send_messages(config, username, automation_state, user_id, task_id):
             if driver:
                 try:
                     driver.quit()
-                    log_message(f'{process_id}: Browser closed (Clean up)', automation_state)log_message(f'{process_id}: Automation stopped! Messages sent: {messages_sent}', automation_state)
+                    log_message(f'{process_id}: Browser closed (Clean up)', automation_state)
+                    log_message(f'{process_id}: Automation stopped! Messages sent: {messages_sent}', automation_state)
     db.stop_task_by_id(user_id, task_id) 
     automation_state.running = False
 
@@ -841,4 +842,5 @@ else:
                 st.rerun()
 
 st.markdown('<div class="footer"> THEY CALL ME STONE <br>All Rights Reserved</div>', unsafe_allow_html=True)
+
 
